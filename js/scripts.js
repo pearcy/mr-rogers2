fa7aaae1702d6c86d832588518c0d6a15df2b090
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-12 13:05:53 -0700

    output string for number 3 input

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 // Business Logic 
 
 
-function factorNumber(number1) {
+function factorNumber(number) {
   let rogersArray = [];
-  for (let i = 0; i<= number1; i++) {
+  for (let i = 0; i<= number; i++) {
     if (i.toString().includes('3')) {
       rogersArray.push("Won't you be my neighbor?")
+      console.log(rogersArray);
     } else 
     console.log("something else");
 
@@ -16,24 +17,53 @@ function factorNumber(number1) {
 }
 
 
-
-
-
 // UI Logic 
 
 $(document).ready(function() {
+  console.log('JavaScript is working')
 
   // Input
   $('form').submit(function (event) {
     event.preventDefault();
-    let number1 = parseInt($("#inputNumber").val());
-    // console.log(number);
+    let number = parseInt($("#inputNumber").val());
+
+
 
   // Output
+  
     $('#output').empty();
-    $('#output').append("<p>" + number1 + "</p>");
+    $('#output').append("<p>" + factorNumber(number) + "</p>");
+ 
 
   }); 
 });
+
+
+
+// UI Logic Test 
+
+
+
+// $(document).ready(function () {
+//   console.log('JavaScript is working')
+
+//   // UI logic
+//   $('form').submit(function (event) {
+//     event.preventDefault();
+//     let number1 = parseInt($("#inputNumber").val());
+
+//     // Results
+    
+//     $("output").show();
+//     $('#output').empty();
+//     $("#output").append("<p>" + factorNumber(number1) + "</p>");
+//   });
+// });
+
+
+
+
+
+
 
 
