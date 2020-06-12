71dd2a6711fc6bc7e334d22373f8787bbb578c40
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-12 15:23:11 -0700

    add logic for entering 2

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,9 @@ function factorNumber(number) {
   for (let i = 0; i<= number; i++) {
     if (i.toString().includes('3')) {
       rogersArray.push("Won't you be my neighbor?")
-      console.log(rogersArray);
-    } else 
+    } else if (i.toString().includes('2')) {
+      rogersArray.push("Boop!")
+    } else
       rogersArray.push(' ' + i);
    
   }
